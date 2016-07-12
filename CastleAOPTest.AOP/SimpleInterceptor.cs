09bd0b681cb72bc24d0ae2207c00e20b75b3f8fa
海底红石 <pingyan_lin@163.com>
2016-07-12 17:45:38 +0800

    AOP拦截器

---
+++ CastleAOPTest.AOP/SimpleInterceptor.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using Castle.Core.Interceptor;
+
+/// <summary>
+/// 重写拦截器方法
+/// </summary>
+namespace CastleAOPTest.AOP
+{
+    public class SimpleInterceptor: StandardInterceptor
+    {
+        protected override void PreProceed(IInvocation invocation)
+        {
+            Console.WriteLine("调用前的拦截器，方法名是：{0}。", invocation.Method.Name);
+            base.PreProceed(invocation);
+
+        }
+
+        protected override void PerformProceed(IInvocation invocation)
+        {
+            Console.WriteLine("拦截的方法返回时调用的拦截器，方法名是：{0}。", invocation.Method.Name);
+            base.PerformProceed(invocation);
+
+        }
+
+
+        protected override void PostProceed(IInvocation invocation)
+        {
+            Console.WriteLine("调用后的拦截器，方法名是：{0}。", invocation.Method.Name);
+            base.PostProceed(invocation);
+
+        }
+    }
+}
---
+++ CastleAOPTest.Run/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CastleAOPTest.AOP;
+using Castle.DynamicProxy;
+
+namespace CastleAOPTest.Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProxyGenerator generator = new ProxyGenerator();//实例化【代理类生成器】  
+            SimpleInterceptor interceptor = new SimpleInterceptor();//实例化【拦截器】  
+
+            //使用【代理类生成器】创建Person对象，而不是使用new关键字来实例化  
+            Person person = generator.CreateClassProxy<Person>(interceptor);
+
+
+            Console.WriteLine("当前类型:{0},父类型:{1}", person.GetType(), person.GetType().BaseType);
+            Console.WriteLine();
+
+
+            person.SayHello();//跟普通调用没有两样吧？  
+            Console.WriteLine();
+
+            person.SayName("福建");//跟普通调用没有两样吧？  
+            Console.WriteLine();
+
+            person.SayOther();//它不是虚方法，无法拦截。待会检测输出情况就知道了。     
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
